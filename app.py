1e782de68eab4a40d5c780334b3fda66778e1e30
Johnathan Booy <johnny_music@icloud.com>
2022-10-04 10:38:12 -0700

    Added flask debug toolbar

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 """Blogly application."""
 
 from flask import Flask, current_app
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 app = Flask(__name__)
+
+# SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+# Flask Debug Toolbar
+app.config['SECRET_KEY'] = "12345"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 db.create_all()
-
-with app.app_context():
-    # within this block, current_app points to app.
-    print(current_app.name)
